d2c069137d4510cc13771c526d04429998f07886
Lars Henrik Nelson <larshnelson@protonmail.com>
2024-04-11 15:25:14 -0400

    remove matplotlib import

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
-#%%
+# %%
 from nidaqworker import NIWorker
 
-#%%
+# %%
 worker = NIWorker("Dev2")
 
-#%%
+# %%
 isi = 10
-repititions=1
+repititions = 1
 # All tasks take samples as input except for isi and iti which are in seconds
 worker.create_ttl_task(
     fs=1,
@@ -17,8 +17,8 @@ worker.create_ttl_task(
     v_range=0.5,
     repititions=repititions,
     isi=isi,
-    channels=[2,3],
-    task_name="baseline"
+    channels=[2, 3],
+    task_name="baseline",
 )
 worker.create_ramp_task(
     fs=1000,
@@ -29,7 +29,7 @@ worker.create_ramp_task(
     min_v=1,
     repititions=repititions,
     isi=isi,
-    channels=[2,3],
+    channels=[2, 3],
 )
 worker.create_sine_task(
     fs=1000,
@@ -41,7 +41,7 @@ worker.create_sine_task(
     v_range=1,
     repititions=repititions,
     isi=isi,
-    channels=[2,3],
+    channels=[2, 3],
 )
 worker.create_ttl_task(
     fs=1000,
@@ -52,17 +52,12 @@ worker.create_ttl_task(
     v_range=3.0,
     repititions=repititions,
     isi=isi,
-    channels=[2,3],
-    task_name="ttl"
+    channels=[2, 3],
+    task_name="ttl",
 )
 
-#%%
+# %%
 worker.run_tasks(iti=5)
 
 # %%
 worker.reset_tasks()
-
-# %%
-import matplotlib.pyplot as plt
-plt.plot(worker.tasks[0].data[0])
-# %%
