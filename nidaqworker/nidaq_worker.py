5a86d09a57419774e5e721b00c270cb5fef8d888
Lars Henrik Nelson <larshnelson@protonmail.com>
2024-04-23 13:08:51 -0400

    added ttl frequency task

--- nidaqworker/nidaq_worker.py
+++ nidaqworker/nidaq_worker.py
@@ -28,6 +28,7 @@ class NIWorker:
         else:
             self.device = device_name
         self.inplace = True
+        self.rng = np.random.default_rng()
 
     def create_sine_task(
         self,
@@ -39,7 +40,7 @@ class NIWorker:
         v_range: Union[float, int] = 10,
         min_v: Union[float, int] = -5,
         repititions: int = 1,
-        isi: Union[int, float] = 0,
+        isi: Union[int, float, tuple[int, float]] = 0,
         channels: Union[list, int] = 0,
         task_name: str = "",
         trigger: str = "",
@@ -72,6 +73,48 @@ class NIWorker:
         if not self.inplace:
             return self
 
+    def create_ttl_freq_task(
+        self,
+        fs: int,
+        f0: int,
+        pulse_t: int,
+        ttl_t: int,
+        ttl_width: int,
+        offset_t: int = 0,
+        v_range: Union[float, int] = 10,
+        min_v: Union[float, int] = -5,
+        repititions: int = 1,
+        isi: Union[int, float, tuple[int, float]] = 0,
+        channels: Union[list, int] = 0,
+        task_name: str = "",
+        trigger: str = "",
+    ):
+        ttl_indexes = np.linspace(
+            0, ttl_t, num=int((ttl_t / fs) * f0), endpoint=False, dtype=int
+        )
+        ttl_data = np.zeros((len(channels), pulse_t))
+        ttl_indexes += offset_t
+        for i in ttl_indexes:
+            for j in range(ttl_width):
+                ttl_data[0, int(i + j)] = v_range
+                ttl_data[1, int(i + j)] = v_range
+
+        ttl_data += min_v
+
+        task = TaskSettings(
+            fs=fs,
+            data=ttl_data,
+            task_name=task_name,
+            repititions=repititions,
+            length=ttl_data.shape[0],
+            isi=isi,
+            trigger=trigger,
+        )
+        self.tasks.append(task)
+
+        if not self.inplace:
+            return self
+
     def create_ramp_task(
         self,
         fs: int,
@@ -81,7 +124,7 @@ class NIWorker:
         v_range: Union[float, int] = 10,
         min_v: Union[float, int] = -5,
         repititions: int = 1,
-        isi: Union[int, float] = 0,
+        isi: Union[int, float, tuple[int, float]] = 0,
         channels: Union[list, int] = 0,
         task_name: str = "",
         trigger: str = "",
@@ -104,7 +147,7 @@ class NIWorker:
             isi=isi,
             trigger=trigger,
         )
-        self.task.append(task)
+        self.tasks.append(task)
 
         if not self.inplace:
             return self
@@ -113,23 +156,23 @@ class NIWorker:
         self,
         fs: int,
         pulse_t: int,
-        ttl_t: int,
+        ttl_width: int,
         offset_t: int = 0,
         v_range: Union[float, int] = 3,
         min_v: Union[float, int] = 0,
         repititions: int = 1,
-        isi: Union[int, float] = 0,
+        isi: Union[int, float, tuple[int, float]] = 0,
         channels: Union[list, int] = 0,
         task_name: str = "",
         trigger: str = "",
     ):
         if isinstance(channels, int):
             channels = [channels]
-        assert (offset_t + ttl_t) < pulse_t
+        assert (offset_t + ttl_width) < pulse_t
 
         ttl_data = np.full((len(channels), pulse_t), min_v)
         for i in range(len(channels)):
-            ttl_data[i, offset_t : int(offset_t + ttl_t)] += v_range
+            ttl_data[i, offset_t : int(offset_t + ttl_width)] += v_range
 
         task = TaskSettings(
             fs=fs,
@@ -140,16 +183,25 @@ class NIWorker:
             isi=isi,
             trigger=trigger,
         )
-        self.task.append(task)
+        self.tasks.append(task)
 
         if not self.inplace:
             return self
 
-    def run_tasks(self, iti: Union[float, int]):
+    def run_tasks(self, iti: Union[float, int, tuple[float, int]]):
         for i, task in enumerate(self.tasks):
             ni_task = nidaqmx.Task(task.task_name)
             if i > 0:
-                time.sleep(iti)
+                if isinstance(iti):
+                    r = iti[1] - iti[0]
+                    tm = self.rng.random(
+                        size=1,
+                    )
+                    tm *= r
+                    tm += iti[0]
+                else:
+                    tm = iti
+                time.sleep(tm)
             self.run_task(ni_task, task)
 
     def run_task(self, ni_task, task_settings: TaskSettings):
@@ -172,7 +224,16 @@ class NIWorker:
         else:
             for i in range(task_settings.repititions):
                 if i > 0:
-                    time.sleep(task_settings.isi)
+                    if isinstance(task_settings.isi):
+                        r = task_settings.isi[1] - task_settings.isi[0]
+                        tm = self.rng.random(
+                            size=1,
+                        )[0]
+                        tm *= r
+                        tm += task_settings.isi[0]
+                    else:
+                        tm = task_settings.isi
+                    time.sleep(tm)
                 ni_task.start()
                 ni_task.wait_until_done(timeout=WAIT_INFINITELY)
                 ni_task.stop()
